3b9d88a77e1d477d75224618d43d5b400f63e93d
ioritz <ioritzurkiri2002@gmail.com>
2022-03-18 10:24:22 +0100

    mi ultimo commit

--- RetoPrueba/src/prueba/Autor.java
+++ RetoPrueba/src/prueba/Autor.java
@@ -1,5 +1,20 @@
 package prueba;
 
 public class Autor {
+	private String dni;
+	private String nombre;
+	private String compania;
+	
+	
+	public Autor(String dni, String nombre, String compania) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.compania = compania;
+	}
 
+
+	public Autor() {
+		super();
+	}
 }
