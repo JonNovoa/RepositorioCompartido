56486e5ae33e94bc0ce3697a305f5e1d2217c135
ioritz <ioritzurkiri2002@gmail.com>
2022-03-18 10:13:12 +0100

    commit del ultimo cambio

--- RetoPrueba/src/prueba/Vendedor.java
+++ RetoPrueba/src/prueba/Vendedor.java
@@ -1,5 +1,53 @@
 package prueba;
 
-public class Vendedor {
+import java.time.LocalDate;
 
+public class Vendedor {
+	private String dni;
+	private LocalDate fContraracion;
+	private int edad;
+	
+	
+	public Vendedor(String dni, LocalDate fContraracion, int edad) {
+		super();
+		this.dni = dni;
+		this.fContraracion = fContraracion;
+		this.edad = edad;
+	}
+
+
+	public String getDni() {
+		return dni;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Vendedor [dni=" + dni + ", edad=" + edad + "]";
+	}
+
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+
+	public LocalDate getfContraracion() {
+		return fContraracion;
+	}
+
+
+	public void setfContraracion(LocalDate fContraracion) {
+		this.fContraracion = fContraracion;
+	}
+
+
+	public int getEdad() {
+		return edad;
+	}
+
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
 }
